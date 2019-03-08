c25e0de171fa976efeedc14e3698621044ef7e22
danielvasilan <daniel_vasilan@yahoo.com>
2019-03-08 19:53:52 +0200

    #9 add config app + templates

--- Django/RunOrg/Mgmt/views.py
+++ Django/RunOrg/Mgmt/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+locations = [
+    {
+        'name' : 'Loc1', 'status' : 'Active'
+    },
+    {
+        'name' : 'Loc2', 'status' : 'Active'
+    },
+    {
+        'name' : 'Loc3', 'status' : 'Closed'
+    }
+]
+
+volunteers = [
+    {
+        'name' : 'Daniel', 'role' : '', 'phone' : '0721723444'
+    }
+
+]
 
 def home(request):
-    return HttpResponse('<h1> Configuration Home </h1>')
+    context = {'locations' : locations, 'volunteers' : volunteers}
+    return render(request, 'Mgmt/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> About Page </h1>')
+    return render(request, 'Mgmt/about.html')
