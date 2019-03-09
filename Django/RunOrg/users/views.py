abf0efe2b582f202e115262bfe82b3d98a6c5abf
danielvasilan <daniel_vasilan@yahoo.com>
2019-03-09 15:41:50 +0200

    #9 login and logout functionality

--- Django/RunOrg/users/views.py
+++ Django/RunOrg/users/views.py
@@ -7,9 +7,9 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username} !')
-            return redirect('mgmt-home')
+            # username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
